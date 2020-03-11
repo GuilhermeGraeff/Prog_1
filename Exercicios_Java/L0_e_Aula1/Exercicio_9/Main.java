278ede00eb34a3306c0f13d54bac91aafbf995a6
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2020-03-11 15:49:56 -0300

    Exercicio dia 11/03/20

---
+++ Exercicios_Java/L0_e_Aula1/Exercicio_9/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Programação I\n  - Professor: Fernando Bevilacqua\n  - Aluno: Seu nome aqui\n  - Objetivo:\n        O objetivo dessa disciplina é compreender os conceitos fundamentais\n        do paradigma de programação orientada a objetos e aplicá-los no\n        desenvolvimento de soluções de software.\n  - Turno: Vespertino");
+        
+    }
+}
